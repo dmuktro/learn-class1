5723f7c84a3db16b6f7070fd62bee78228f56333
dmuktro <dmuk2020@hotmail.com>
2015-07-24 16:06:43 +0300

    Create simple-script.py

---
+++ simple-script.py
@@ -0,0 +1,35 @@
+# from (c) https://www.codecademy.com/courses/python-beginner-en-kSQwt/ and Thanks TO http://bogdankulynych.me/  
+
+
+# 1\19
+favorite_number = 111
+print favorite_number*favorite_number*favorite_number 
+
+
+# 2\19
+name = "Jessica"
+location = "Grendel's Cupcake Den"
+time = "7:30pm"
+
+print("You are invited to a birthday party for " + name)
+print("The party will be at " + location + " and starts at " + time)
+
+
+# 3\19
+monday_vegetable = "asparagus"
+monday_entree = "pasta primavera"
+
+tuesday_vegetable = "artichoke"
+tuesday_entree = "steak frites"
+
+print("Monday's specials are " + monday_vegetable + " and " + monday_entree)
+print("Tuesday's specials are " + tuesday_vegetable + " and " + tuesday_entree) 
+
+
+# 4\19
+# is it correct solution?
+html = "Test message"
+
+print("<i>" + html + "</i>")
+print("<code>" + html + "</code>")
+print("<h1>" + html + "</h1>")
