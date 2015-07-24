17bc405276b35ced18c0c556a8d8c186353dda8a
Dmytro <dmuk2020@hotmail.com>
2015-07-24 20:41:51 +0300

    Update simple-script.py

--- simple-script.py
+++ simple-script.py
@@ -27,9 +27,51 @@ print("Tuesday's specials are " + tuesday_vegetable + " and " + tuesday_entree)
 
 
 # 4\19
-# is it correct solution?
 html = "Test message"
 
-print("<i>" + html + "</i>")
-print("<code>" + html + "</code>")
-print("<h1>" + html + "</h1>")
+print ("<b>" + html + "</b>")
+print ("<code>" + html + "</code>")
+print ("<h1>" + html + "</h1>")
+
+
+# 5\19
+# On the weekends, you get to sleep in! 
+today = "Saturday"
+
+if (today=="Saturday" or today=="Sunday"):  print("SLEEPING IN")
+
+
+# 6\19 
+# It takes 125 ladybugs to protect 1 tree from aphids. Do we have enough ladybugs to protect our orchard? 
+ladybugs = 2600
+trees = 20
+
+if ladybugs/125 >= trees: 
+    print("OK") 
+else: 
+    print("NOT OK") 
+
+
+# 7\19 
+# We are composing some Tweets for our Twitter followers, but are stuck on the fact that Twitter messages can't be more than 140 characters! We need to only print our messages if they are short enough for Twitter.
+message1 = "The Analytical Engine weaves algebraic patterns, just as the Jacquard loom weaves flowers and leaves. -- Ada Lovelace, the first programmer"
+message2 = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
+message3 = "They told me computers could only do arithmetic. -- Computer pioneer Grace Hopper"
+
+if len(message1) <= 140:
+    print (message1) 
+if len(message2) <= 140:
+    print (message2) 
+if len(message3) <= 140:
+    print (message3) 
+
+
+# 8\19 
+# 
+# ??
+
+if (good_weather and on_vacation and no_bears):
+    print ("camping")
+else:
+    print ("staying home") 
+
