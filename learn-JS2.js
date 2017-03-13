9dc25e78a3851a397808e5fa2c7d2f5a8e1431a9
Dmytro <dmuk2020@hotmail.com>
2017-03-13 20:41:49 +0200

    Update learn-JS2.js

--- learn-JS2.js
+++ learn-JS2.js
@@ -1,7 +1,15 @@
+var orderCount = 0;
+function getSubTotal(itemCount) {
+  return(itemCount * 7.5)
+}
+
 function takeOrder(topping, crustType) {
   console.log('Order: ' + crustType + ' pizza topped with ' + topping ); 
+  orderCount = orderCount + 1;
 }
 
 takeOrder('bacon','thin');
 takeOrder('cheese','thin');
 takeOrder('bacon','thick');
+
+console.log(getSubTotal(orderCount));
