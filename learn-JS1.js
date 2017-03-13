5eebc1fdd0f9f44b8aa0fff2d7e3c0cc220c2292
Dmytro <dmuk2020@hotmail.com>
2017-03-13 19:30:44 +0200

    Update learn-JS1.js

--- learn-JS1.js
+++ learn-JS1.js
@@ -34,3 +34,16 @@ switch (moonPhase) {
   default: 
     console.log('Invalid moon phase'); break; 
 }
+
+//
+// if/else statements make binary decisions and execute separate code based on a condition.
+// We can add extra conditions with to if/else statements with else if conditions.
+// switch statements make complicated if/else statements easier to read, however they achieve the same result as if/else statements.
+// Comparison operators, like <, >, <=, and >= can compare two variables. After they compare, they always return either true or false.
+// Logical Operators, like &&, ||, !==, and !, can compare two variables to see if a certain condition exists:
+// && checks if both sides are true.
+// || checks if either side is true.
+// !== checks if both sides are not equal.
+// ! changes a variable that is true to false, and vice versa.
+// In the next lesson, we'll learn about functions, and how to write blocks of code that are reusable.
+//
