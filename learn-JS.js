97b0230a0b3d286548f577b7cf9449088ba2c7ec
Dmytro <dmuk2020@hotmail.com>
2017-03-13 16:12:31 +0200

    Update learn-JS.js

--- learn-JS.js
+++ learn-JS.js
@@ -1,8 +1,3 @@
-var strength = '50,000 pounds';
-var age = 45;
-var hasPet = true;
-
-console.log(strength);
-console.log(age);
-console.log(hasPet);
-// Output all the varS
+var morningAlarm = '6:30AM';
+morningAlarm = '9:30AM';
+console.log('Morning alarm is set to: ', morningAlarm);
