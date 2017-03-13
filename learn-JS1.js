8a7a6f7152c3adab169676e18c1d6866535fbe9e
Dmytro <dmuk2020@hotmail.com>
2017-03-13 19:28:37 +0200

    Update learn-JS1.js

--- learn-JS1.js
+++ learn-JS1.js
@@ -22,3 +22,15 @@ if (moonPhase === 'full' || foggyNight) {
 } else { 
   console.log('Invalid moon phase');
 }
+
+var moonPhase = 'full';
+switch (moonPhase) {
+  case 'full':
+    console.log('Howwwwlll!!'); break;
+  case 'mostly full': 
+    console.log('Arms and legs are getting hairier'); break;
+  case 'mostly new':
+    console.log('Back on two feet'); break;
+  default: 
+    console.log('Invalid moon phase'); break; 
+}
