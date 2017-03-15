b5c322a982b336645e5df50581691e956538fe34
Dmytro <dmuktro@users.noreply.github.com>
2017-03-15 19:45:35 +0200

    Update learn-JS4.js

--- learn-JS4.js
+++ learn-JS4.js
@@ -1,1 +1,10 @@
+var laundryRoom = 'Basement';
+var mailRoom = 'Room 1A';
 
+function myApartment() {
+  var mailBoxNumber = "Box 3";
+  laundryRoom = 'In-unit';
+  console.log('Mail box: ' + mailBoxNumber + ', Laundry:' + laundryRoom);
+}
+
+console.log(mailBoxNumber);
