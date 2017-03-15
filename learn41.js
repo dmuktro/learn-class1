25f249ae7a6a95bf21b6504b6eaf0a8c2776ffe1
Dmytro <dmuktro@users.noreply.github.com>
2017-03-15 19:49:03 +0200

    Create learn41.js

---
+++ learn41.js
@@ -0,0 +1,22 @@
+function myApartment() {
+	var myCoffeeMaker = 'Aeropress';
+	var buildingAddress = '150 E 14th St, New York, NY';
+	var myCloset = 'Extra coats in the back';
+	var buildingLaundryCode = 4927;
+	var myRefridgerator = 'Filled with veggies and dark chocolate.';
+	var myDog = 'Nikko';
+	var buildingPhone = '(481) 516-2342';
+}
+
+
+
+
+
+
+
+
+// Do not edit the code after this line
+console.log("**Apartment Building Information**");
+console.log("Laundry code: " + buildingLaundryCode + "\nPhone: " + buildingPhone + "\nMailing address: " + buildingAddress);
+
+// ERR !
