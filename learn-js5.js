a21e5092cba59e09215a8201d8e7e0967e817d1b
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 15:01:29 +0200

    Update learn-js5.js

--- learn-js5.js
+++ learn-js5.js
@@ -1,2 +1,7 @@
 var bucketList = ['s1', 's2', 's3'];
 console.log(bucketList);
+var listItem = bucketList[0]; 
+console.log(listItem);
+listItem = bucketList[2]; 
+listItem = bucketList[3]; 
+console.log(listItem);
