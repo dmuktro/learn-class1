5ea6966e4d899fdaf8ac2348c2afd1d98b68f287
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 17:35:38 +0200

    Update learn-js7_DOM.js

--- learn-js7_DOM.js
+++ learn-js7_DOM.js
@@ -1,2 +1,5 @@
-var skillset = document.getElementsByClassName('skillset');
-alert(skillset);
+// var skillset = document.getElementsByClassName('skillset');
+// alert(skillset);
+function main() {
+  
+}
