bc2604ff1efe91b0a1f95f0dbd981719481765bb
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 16:05:54 +0200

    Update learn-js6_loops.js

--- learn-js6_loops.js
+++ learn-js6_loops.js
@@ -1,5 +1,5 @@
 var vacationSpots = ['Carpatians', 'Fiji', "Lima"];
 
-console.log(vacationSpots[0]);
-console.log(vacationSpots[1]);
-console.log(vacationSpots[2]);
+for (var i = 0; i < vacationSpots.length; i++) {
+  console.log('I would love to visit ' +  vacationSpots[i] + ', then');
+}
