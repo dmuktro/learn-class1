b3f9959653d860ed7824b2ef06549ff42bb762ad
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 16:36:35 +0200

    Update learn-js6_loops.js

--- learn-js6_loops.js
+++ learn-js6_loops.js
@@ -9,3 +9,15 @@ var vacationSpots = ['Carpatians', 'Fiji', "Lima"];
 for (var i = vacationSpots.length-1; i >= 0 ; i--) {
   console.log('I would love to visit ' +  vacationSpots[i] );
 }
+
+// add &comparision 
+var friendPlaces = ['Alps', 'Cuba', "Oslo"];
+
+for (var i = 0; i < myPlaces.length; i++) {
+  console.log( myPlaces[i] );
+  for (var j = 0; j < friendPlaces.length; j++) {
+    if (myPlaces[i] === friendPlaces[j]) {
+      console.log( myPlaces[i] )
+    } else ; 
+  }
+}
