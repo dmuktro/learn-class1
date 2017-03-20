31231635cab99c45a10b01eed5768aae47b49260
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 16:08:01 +0200

    Update learn-js6_loops.js

--- learn-js6_loops.js
+++ learn-js6_loops.js
@@ -3,3 +3,9 @@ var vacationSpots = ['Carpatians', 'Fiji', "Lima"];
 for (var i = 0; i < vacationSpots.length; i++) {
   console.log('I would love to visit ' +  vacationSpots[i] + ', then');
 }
+// &viceversa 
+var vacationSpots = ['Carpatians', 'Fiji', "Lima"];
+
+for (var i = vacationSpots.length-1; i >= 0 ; i--) {
+  console.log('I would love to visit ' +  vacationSpots[i] );
+}
