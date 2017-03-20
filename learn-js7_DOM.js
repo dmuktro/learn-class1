f616b26beb04bb0d8f91f45140ece494dc2df198
Dmytro <dmuktro@users.noreply.github.com>
2017-03-20 18:03:35 +0200

    Update learn-js7_DOM.js

--- learn-js7_DOM.js
+++ learn-js7_DOM.js
@@ -1,8 +1,11 @@
 // var skillset = document.getElementsByClassName('skillset');
 // alert(skillset);
 function main() {
+  var $skillset = $('.skillset');
+  alert($skillset);
 }
-$(document).ready(main);  
+
+$(document).ready(main);
 
 // document.getElementsByClassName('skillset');
 //   With jQuery we can select the same element with:
