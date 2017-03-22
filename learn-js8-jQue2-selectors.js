417e5fe51b8b7f253d197c400678c6676d8200c1
Dmytro <dmuktro@users.noreply.github.com>
2017-03-22 20:08:13 +0200

    Update learn-js8-jQue2-selectors.js

--- learn-js8-jQue2-selectors.js
+++ learn-js8-jQue2-selectors.js
@@ -1,9 +1,13 @@
 // We can attach next to any jQuery selector to select the next direct element.
+// jQuery provides a method named slideToggle that can animate an element's entrance and exit. 
+
 function main() {
   $('.projects').hide(); 
   $('.projects-button').on('click', 
-    function() { $(this).next().toggle(); 
-      $(this).text('Projects reViewed')
+    function() { 
+//      $(this).next().toggle(); 
+      $(this).text('Projects reViewed');
+      $(this).next().slideToggle(700);
     });
 }
 
