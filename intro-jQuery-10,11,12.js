7900770f7724f50e238d0659f30c2c493697f97d
Dmytro <dmuktro@users.noreply.github.com>
2017-04-05 20:42:35 +0300

    Update and rename intro-jQuery-10,11,12.js to intro-jQuery-10panel11,12.js

--- intro-jQuery-10,11,12.js
+++
@@ -1,7 +0,0 @@
-//  
-
-$(document).ready(function() {
-    $('div').click(function() {
-        $(this).fadeOut('slow');
-    });
-});
---
+++ intro-jQuery-10panel11,12.js
@@ -0,0 +1,7 @@
+//  https://www.codecademy.com/en/courses/web-beginner-en-GfjC6/2/3?
+
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('.pull-me').click($('.panel').slideToggle('slow'));
+        }) 
+});
