ab2d663beaf979c32a2b0a9cd9d30a1c7a2551c9
Dmytro <dmuktro@users.noreply.github.com>
2017-04-05 20:09:55 +0300

    Create intro-jq7.js

---
+++ intro-jq7.js
@@ -0,0 +1,7 @@
+//  https://www.codecademy.com/en/courses/web-beginner-en-GfjC6/1/3?
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('div#blue').fadeOut('slow');
+    });
+});
