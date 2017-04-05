bf4fffb1a0fe71e26a5a820fae2cb2c5a20d2607
Dmytro <dmuktro@users.noreply.github.com>
2017-04-05 20:23:03 +0300

    Create intro-jq9this.js

---
+++ intro-jq9this.js
@@ -0,0 +1,7 @@
+//  https://www.codecademy.com/en/courses/web-beginner-en-GfjC6/1/5?
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).fadeOut('slow');
+    });
+});
