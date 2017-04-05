7091f7c5a463ca2ca3c1a49f8bbdc25e3c370e76
Dmytro <dmuktro@users.noreply.github.com>
2017-04-05 18:06:06 +0300

    Create intro-jQuery-3variables.js

---
+++ intro-jQuery-3variables.js
@@ -0,0 +1,7 @@
+// https://www.codecademy.com/courses/web-beginner-en-GfjC6/0/3
+// Write your jQuery code on line 3!
+
+$(document).ready(function() {
+    var $target = $('li');
+    $target.fadeOut('fast');
+});
