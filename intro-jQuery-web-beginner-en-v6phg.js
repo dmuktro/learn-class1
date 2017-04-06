75b14ffcb86168ac078c0b3a63601b2862b56fda
Dmytro <dmuktro@users.noreply.github.com>
2017-04-06 18:34:33 +0300

    Create intro-jQuery-web-beginner-en-v6phg.js

---
+++ intro-jQuery-web-beginner-en-v6phg.js
@@ -0,0 +1,21 @@
+// https://www.codecademy.com/en/courses/web-beginner-en-v6phg/2/4?
+//  create the variable toAdd and set it equal to the contents of the input field
+//   and .append() a <div> with class="item" to the .list div of your HTML document
+//    by adding the .on() event handler as described above. It should 
+//    go inside your $(document).ready(), but after and outside your $('#button').click().
+$(document).ready(function() {
+    $('#button').on('click', function() {
+		var toAdd = $('input[name=checkListItem]').val();
+		$('.list').append('<div class="item">' + toAdd + '</div>');
+    });
+});
+
+$(document).ready(function() {
+    $('#button').on('click', function() {
+		var toAdd = $('input[name=checkListItem]').val();
+		$('.list').append('<div class="item">' + toAdd + '</div>');
+    });
+    $(document).on('click', '.item',function() {
+		$(this).remove();
+    });
+});
