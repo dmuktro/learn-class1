029789a6d640601283576a67a924c892af3b350a
Dmytro <dmuktro@users.noreply.github.com>
2017-04-07 20:37:31 +0300

    Create intro-jQuery-6_codeAcademy.js

---
+++ intro-jQuery-6_codeAcademy.js
@@ -0,0 +1,10 @@
+// https://www.codecademy.com/
+$(document).ready(function() {
+    // add the code here
+});
+
+
+//  https://www.codecademy.com/en/courses/web-beginner-en-jtFIC/1/1?
+$(document).ready(function() {
+    $("#menu").accordion({collapsible: true, active: false});
+});
