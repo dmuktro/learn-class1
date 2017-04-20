7e4415bb12f5741e1cb51218cf9eca6496bcf0e6
Dmytro <dmuktro@users.noreply.github.com>
2017-04-20 17:50:10 +0300

    Rename intro-jQuery-7_cA.js to intro-jQuery-9_cA.js

---
+++ intro-jQuery-9_cA.js
@@ -0,0 +1,6 @@
+//  https://www.codecademy.com/courses/web-beginner-en-jtFIC/1/4?
+$(document).ready(function() {
+    // $('ol').selectable();
+//  either of 'behavious' of LI-s are activated , BUT not both.. 
+    $('ol').sortable();
+});
